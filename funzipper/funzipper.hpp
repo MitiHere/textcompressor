cf57993ceded61d84e0a6e066600123306e7b689
MitiHere <ce.eyraud@gmail.com>
2023-10-16 15:19:19 +0200

    smlfix

--- funzipper/funzipper.hpp
+++ funzipper/funzipper.hpp
@@ -2,7 +2,6 @@
 #define FUNZIPPER_HPP
 #include "vectoredTxt.hpp"
 #include "uwordmap.hpp"
-
 class funzipper
 {
 public:
@@ -10,11 +9,17 @@ public:
 
     funzipper(std::string filename) : vt(filename)
     {
-        wm = uwordmap(vt);
+        wm = uwordmap(vt, WM_MIN_COUNTER);
     };
 
 private:
     uwordmap wm;
     vectoredTxt vt;
+
+    const int WM_START = 1;
+    const int WM_END = 2;
+    const int TXT_START = 3;
+    const int TXT_END = 4;
+    const int WM_MIN_COUNTER = 5;
 };
 #endif
