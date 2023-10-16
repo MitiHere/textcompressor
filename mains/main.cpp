094523d33e1bb8aee19728b850e40e086ddb30fe
MitiHere <ce.eyraud@gmail.com>
2023-10-16 14:42:16 +0200

    cleanup

--- mains/main.cpp
+++ mains/main.cpp
@@ -1,16 +1,7 @@
-/*#include "../funzipper/vectoredTxt.hpp"
-#include "../funzipper/uwordmap.hpp"
-*/
 #include "../funzipper/funzipper.hpp"
 int main()
 {
     funzipper fz = funzipper("text.txt");
-    /*
-    vectoredTxt vt = vectoredTxt("text.txt");
-    uwordmap um = uwordmap(vt);
-    vt.print();
-    um.print();
-    */
     fz.print();
     return 0;
 }
